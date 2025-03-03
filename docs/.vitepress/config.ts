66db08273215162e5b62ee09af84af7dfcf1aca4
Aylur <k.demeter@protonmail.com>
2025-03-03 02:07:30 +0100

    fix base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress"
 
 export default defineConfig({
     title: "Gjsx",
-    base: "/astal/",
+    base: "/gjsx/",
     themeConfig: {
         outline: "deep",
 
