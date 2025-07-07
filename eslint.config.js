9bffa83f52f711b13e3c139454623a9aea4f5552
Aylur <k.demeter@protonmail.com>
2025-07-08 00:55:58 +0200

    chore: update lockfiles

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
-        ignores: ["docs/.vitepress/"],
+        ignores: ["docs/.vitepress/", "dist/"],
     },
     {
         rules: {
