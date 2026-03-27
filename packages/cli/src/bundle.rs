33993b718d33be42aaf8e59362fe2aebf5758bfa
Aylur <k.demeter@protonmail.com>
2026-03-27 17:06:47 +0100

    feat(bundle): exe option

--- packages/cli/src/bundle.rs
+++ packages/cli/src/bundle.rs
@@ -2,13 +2,15 @@ use crate::dev::ModuleTracker;
 use crate::plugin::{css::GnimCssPlugin, resource::GnimResourcePlugin};
 use crate::{dev_rundir, rolldown_config};
 use clap::Args;
-use std::sync::Arc;
+use std::os::unix::fs::PermissionsExt;
+use std::path;
+use std::{env, fs, sync::Arc};
 
 #[derive(Args)]
 pub struct BundleArgs {
-    /// Entry module
+    /// Entry module or gresource location for the executable
     pub entry: String,
-    /// Target gresource bundle
+    /// Output file
     pub outfile: String,
     /// Replace global identifiers with constant expressions
     #[arg(short, long, value_name = "KEY=VALUE", value_parser = crate::parse_key_val)]
@@ -19,9 +21,66 @@ pub struct BundleArgs {
     /// Path prefix used in the gresource
     #[arg(short, long, value_name = "PATH", default_value_t = String::from("/"))]
     pub prefix: String,
+    /// Create an executable for a given gresource
+    #[arg(short, long)]
+    pub exe: bool,
 }
 
 pub async fn bundle(args: &BundleArgs) -> Result<(), String> {
+    if !args.prefix.ends_with("/") || !args.prefix.starts_with("/") {
+        return Err("prefix should end and start with a slash '/'".into());
+    }
+
+    if args.exe {
+        let outfile = path::Path::new(&args.outfile);
+        let outdir = outfile.parent().expect("Invalid outfile");
+        fs::create_dir_all(outdir).expect("Failed to create directories");
+
+        let gresource = {
+            let entry = path::Path::new(&args.entry);
+            if entry.is_absolute() {
+                entry.to_path_buf()
+            } else {
+                env::current_dir()
+                    .expect("Failed to read current_dir")
+                    .join(entry)
+            }
+        };
+
+        let gjs = env::var_os("PATH").and_then(|paths| {
+            env::split_paths(&paths)
+                .map(|dir| dir.join("gjs"))
+                .find(|path| path.is_file())
+        });
+
+        let main = format!("resource://{}{}", args.prefix, args.main_alias);
+
+        return match gjs {
+            None => Err("Failed to find gjs in $PATH".into()),
+            Some(gjs) => {
+                let mut content = String::new();
+                content.push_str(&format!("#!{} -m\n", gjs.to_string_lossy()));
+                content.push_str("import Gio from \"gi://Gio\"\n");
+                content.push_str(&format!(
+                    "const r = Gio.Resource.load({:?})\n",
+                    gresource.to_string_lossy(),
+                ));
+                content.push_str("r._register()\n");
+                content.push_str(&format!("await import({:?})\n", main));
+
+                fs::write(outfile, content).expect("Failed to write file");
+
+                let mut perms = fs::metadata(outfile)
+                    .expect("Failed to get metadata for outfile")
+                    .permissions();
+                perms.set_mode(perms.mode() | 0o111);
+                fs::set_permissions(outfile, perms).expect("Failed set file permissions");
+
+                Ok(())
+            }
+        };
+    }
+
     let tracker = ModuleTracker::new(&args.entry).await?;
     let resources = Arc::new(GnimResourcePlugin::new(Some(args.prefix.clone())));
 
